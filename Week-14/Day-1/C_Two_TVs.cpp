5c079cf66532163bf182554c01ddb50c674ee0d5
rayhaniloy <rayhaniloy@gmail.com>
2025-01-30 23:02:48 +0600

    Add files via upload
    week 14 added

---
+++ Week-14/Day-1/C_Two_TVs.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    map<int, int> d;
+    for (int i = 1; i <= n; i++)
+    {
+        int l, r;
+        cin >> l >> r;
+        d[l]++;
+        d[r + 1]--;
+    }
+    long long sum = 0;
+    bool ans = true;
+    for (auto [key, value] : d)
+    {
+        sum += value;
+        if (sum > 2)
+        {
+            ans = false;
+            break;
+        }
+    }
+    if (ans)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
