7561cb045cf6b129ea27732b6baf28ae1888ebd8
rayhaniloy <rayhaniloy@gmail.com>
2025-02-23 23:42:19 +0600

    Add files via upload
    week 16 updated

---
+++ Week-16/Day-6/D_Districts_Connection.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n + 1);
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> a[i];
+        }
+
+        bool ok = false;
+        for (int i = 1; i <= n; i++)
+        {
+            if (a[1] != a[i])
+            {
+                ok = true;
+                break;
+            }
+        }
+
+        if (ok)
+        {
+            int disctrict = -1;
+            cout << "YES" << endl;
+            for (int i = 2; i <= n; i++)
+            {
+                if (a[1] != a[i])
+                {
+                    cout << 1 << " " << i << endl;
+                    disctrict = i;
+                }
+            }
+
+            for (int i = 2; i <= n; i++)
+            {
+                if (a[i] == a[1])
+                {
+                    cout << disctrict << " " << i << endl;
+                }
+            }
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
